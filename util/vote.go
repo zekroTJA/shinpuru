e091a0cf53588b4d0e7a92d109d2efdacadab322
zekro <contact@zekro.de>
2019-01-05 16:32:07 +0100

    added vote command (missing database safe)

---
+++ util/vote.go
@@ -0,0 +1,134 @@
+package util
+
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/gob"
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var VotesRunning = map[string]*Vote{}
+
+var VoteEmotes = strings.Fields("\u0031\u20E3 \u0032\u20E3 \u0033\u20E3 \u0034\u20E3 \u0035\u20E3 \u0036\u20E3 \u0037\u20E3 \u0038\u20E3 \u0039\u20E3 \u0030\u20E3")
+
+type Vote struct {
+	ID            string
+	MsgID         string
+	CreatorID     string
+	GuildID       string
+	ChannelID     string
+	Description   string
+	Possibilities []string
+	Ticks         []*VoteTick
+}
+
+type VoteTick struct {
+	UserID string
+	Tick   int
+}
+
+func VoteUnmarshal(data string) (*Vote, error) {
+	rawData, err := base64.StdEncoding.DecodeString(data)
+	if err != nil {
+		return nil, err
+	}
+
+	var res Vote
+	buffer := bytes.NewBuffer(rawData)
+	gobdec := gob.NewDecoder(buffer)
+	err = gobdec.Decode(&res)
+	return &res, err
+}
+
+func (v *Vote) Marshal() (string, error) {
+	var buffer bytes.Buffer
+	gobenc := gob.NewEncoder(&buffer)
+	err := gobenc.Encode(v)
+	if err != nil {
+		return "", err
+	}
+	gobres := buffer.Bytes()
+	res := base64.StdEncoding.EncodeToString(gobres)
+	return res, nil
+}
+
+func (v *Vote) AsEmbed(s *discordgo.Session, closed bool) (*discordgo.MessageEmbed, error) {
+	creator, err := s.User(v.CreatorID)
+	if err != nil {
+		return nil, err
+	}
+	openStr := "Open"
+	color := ColorEmbedDefault
+	if closed {
+		openStr = "Closed"
+		color = ColorEmbedOrange
+	}
+
+	totalTicks := make(map[int]int)
+	for _, t := range v.Ticks {
+		if _, ok := totalTicks[t.Tick]; !ok {
+			totalTicks[t.Tick] = 1
+		} else {
+			totalTicks[t.Tick]++
+		}
+	}
+	ticksStr := v.Description + "\n\n"
+	for i, p := range v.Possibilities {
+		ticksStr += fmt.Sprintf("%s    %s  -  `%d`\n", VoteEmotes[i], p, totalTicks[i])
+	}
+
+	return &discordgo.MessageEmbed{
+		Color: color,
+		Title: openStr + " Vote",
+		Author: &discordgo.MessageEmbedAuthor{
+			IconURL: creator.AvatarURL("16x16"),
+			Name:    creator.Username + "#" + creator.Discriminator,
+		},
+		Description: ticksStr,
+		Footer: &discordgo.MessageEmbedFooter{
+			Text: "VoteID: " + v.ID,
+		},
+	}, nil
+}
+
+func (v *Vote) AddReactions(s *discordgo.Session) error {
+	for i := 0; i < len(v.Possibilities); i++ {
+		err := s.MessageReactionAdd(v.ChannelID, v.MsgID, VoteEmotes[i])
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (v *Vote) Tick(s *discordgo.Session, userID string, tick int) error {
+	for _, t := range v.Ticks {
+		if t.UserID == userID {
+			return errors.New("votedTwice")
+		}
+	}
+	v.Ticks = append(v.Ticks, &VoteTick{
+		UserID: userID,
+		Tick:   tick,
+	})
+	emb, err := v.AsEmbed(s, false)
+	if err != nil {
+		return err
+	}
+	_, err = s.ChannelMessageEditEmbed(v.ChannelID, v.MsgID, emb)
+	return err
+}
+
+func (v *Vote) Close(s *discordgo.Session) error {
+	delete(VotesRunning, v.ID)
+	emb, err := v.AsEmbed(s, true)
+	if err != nil {
+		return err
+	}
+	_, err = s.ChannelMessageEditEmbed(v.ChannelID, v.MsgID, emb)
+	return err
+}
