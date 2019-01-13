82c1127de925c02dd38db60f83b9974cbe5ec813
JahisanaMalimimbra <46654110+JahisanaMalimimbra@users.noreply.github.com>
2019-01-13 19:10:16 +0100

    Update installdeps.sh

--- scripts/installdeps.sh
+++ scripts/installdeps.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-go get github.com/bwmarrin/discordgog opkg.in/yaml.v2 github.com/go-sql-driver/mysql github.com/op/go-logging github.com/bwmarrin/snowflake
+go get github.com/bwmarrin/discordgo gopkg.in/yaml.v2 github.com/go-sql-driver/mysql github.com/op/go-logging github.com/bwmarrin/snowflake
