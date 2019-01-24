254c0fbad1313e3940eeb67aed20a053af9cc8c4
zekroTJA <riho@live.de>
2019-01-24 22:31:00 +0100

    fixed travis build script

--- scripts/getsqlschemes.bash
+++ scripts/getsqlschemes.bash
@@ -4,5 +4,5 @@ MYSQLSCHEME=$(cat scripts/mysqlDbScheme.sql | base64 -w 0)
 SQLITESCHEME=$(cat scripts/sqliteDbScheme.sql | base64 -w 0)
 
 SQLLDFLAGS="\
-    -X github.com/zekroTJA/shinpuru/core.MySqlDbSchemeB64=$MYSQLSCHEME \
-    -X github.com/zekroTJA/shinpuru/core.SqliteDbSchemeB64=$SQLITESCHEME "
+    -X github.com/zekroTJA/shinpuru/internal/core.MySqlDbSchemeB64=$MYSQLSCHEME \
+    -X github.com/zekroTJA/shinpuru/internal/core.SqliteDbSchemeB64=$SQLITESCHEME "
