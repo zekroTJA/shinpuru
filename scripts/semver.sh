f1edede7a2059086f96431142cea312bfa1174e9
zekroTJA <riho@live.de>
2020-07-08 00:02:32 +0200

    fix semantic versioning in docker build [fix #118]

---
+++ scripts/semver.sh
@@ -0,0 +1,7 @@
+VERSION=$(git describe --tags --abbrev=0)
+NCOMMS=$(git log $VERSION..HEAD --oneline | wc -l)
+LASTCOMM=$(git rev-parse HEAD)
+
+[ "$NCOMMS" == 0 ] \
+    && echo "$VERSION" \
+    || echo "$VERSION+$NCOMMS-${LASTCOMM:0:6}"
