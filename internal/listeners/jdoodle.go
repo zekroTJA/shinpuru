a075670b4bcf00d801b55563b77850d48dbfe825
zekroTJA <riho@live.de>
2020-07-19 11:41:27 +0200

    rework jdoodle listener [fix #53]

--- internal/listeners/jdoodle.go
+++ internal/listeners/jdoodle.go
@@ -21,7 +21,10 @@ import (
 )
 
 const (
-	removeHandlerTimeout   = 3 * time.Minute
+	removeHandlerTimeout         = 3 * time.Minute
+	removeHandlerCleanupInterval = 1 * time.Minute
+
+	// Ratelimiter settings
 	limitTMCleanupInterval = 30 * time.Second // 10 * time.Minute
 	limitTMLifetime        = 24 * time.Hour
 	limitBurst             = 2                // 5
@@ -54,6 +57,15 @@ var (
 type ListenerJdoodle struct {
 	db     database.Database
 	limits *timedmap.TimedMap
+	msgMap *timedmap.TimedMap
+}
+
+type jdoodleMessage struct {
+	*discordgo.Message
+
+	rb *jdoodleRequestBody
+
+	embLang string
 }
 
 type jdoodleRequestBody struct {
@@ -77,11 +89,20 @@ func NewListenerJdoodle(db database.Database) *ListenerJdoodle {
 	return &ListenerJdoodle{
 		db:     db,
 		limits: timedmap.New(limitTMCleanupInterval),
+		msgMap: timedmap.New(removeHandlerCleanupInterval),
 	}
 }
 
-func (l *ListenerJdoodle) Handler(s *discordgo.Session, e *discordgo.MessageCreate) {
-	if e.Author.Bot || e.GuildID == "" {
+func (l *ListenerJdoodle) HandlerMessageCreate(s *discordgo.Session, e *discordgo.MessageCreate) {
+	l.handler(s, e.Message)
+}
+
+func (l *ListenerJdoodle) HandlerMessageUpdate(s *discordgo.Session, e *discordgo.MessageUpdate) {
+	l.handler(s, e.Message)
+}
+
+func (l *ListenerJdoodle) handler(s *discordgo.Session, e *discordgo.Message) {
+	if e.Author == nil || e.Author.Bot || e.GuildID == "" {
 		return
 	}
 
@@ -138,124 +159,131 @@ func (l *ListenerJdoodle) Handler(s *discordgo.Session, e *discordgo.MessageCrea
 		return
 	}
 
-	var removeHandler func()
-
-	removeHandler = s.AddHandler(func(_ *discordgo.Session, eReact *discordgo.MessageReactionAdd) {
-		if eReact.UserID == s.State.User.ID || eReact.GuildID != e.GuildID || eReact.MessageID != e.ID {
-			return
-		}
-
-		if eReact.Emoji.Name != runReactionEmoji {
-			return
-		}
-
-		if !l.checkLimit(eReact.UserID) {
-			s.MessageReactionRemove(eReact.ChannelID, eReact.MessageID, eReact.Emoji.Name, eReact.UserID)
-			return
-		}
-
-		s.MessageReactionsRemoveAll(eReact.ChannelID, eReact.MessageID)
-		removeHandler()
-
-		resMsg := util.SendEmbed(s, eReact.ChannelID, "Executing...", "", static.ColorEmbedGray)
-		if resMsg.Error() != nil {
-			return
-		}
-
-		requestBody := &jdoodleRequestBody{
+	jdMsg := &jdoodleMessage{
+		Message: e,
+		rb: &jdoodleRequestBody{
 			ClientID:     jdCredsSplit[0],
 			ClientSecret: jdCredsSplit[1],
 			Script:       cont,
 			Language:     lang,
-		}
+		},
+		embLang: embLang,
+	}
 
-		bodyBuffer, err := json.Marshal(requestBody)
-		if err != nil {
-			unexpectedError(s, eReact.ChannelID, err)
-			return
-		}
-		req, err := http.NewRequest("POST", apiURL, bytes.NewReader(bodyBuffer))
-		if err != nil {
-			unexpectedError(s, eReact.ChannelID, err)
-			return
-		}
-		req.Header.Add("Content-Type", "application/json")
-
-		res, err := http.DefaultClient.Do(req)
-		if err != nil {
-			unexpectedError(s, eReact.ChannelID, err)
-			return
-		}
-
-		dec := json.NewDecoder(res.Body)
-		if res.StatusCode != 200 {
-			errBody := new(jdoodleResponseError)
-			err = dec.Decode(errBody)
-
-			if err != nil {
-				unexpectedError(s, eReact.ChannelID, err)
-				return
-			}
-
-			s.ChannelMessageEditEmbed(resMsg.ChannelID, resMsg.ID, &discordgo.MessageEmbed{
-				Color:       static.ColorEmbedError,
-				Title:       "Execution Error",
-				Description: fmt.Sprintf("API responded with following error: ```\nCode: %d\nMsg:  %s\n```", res.StatusCode, errBody.Error),
-			})
-			discordutil.DeleteMessageLater(s, resMsg.Message, 15*time.Second)
-
-		} else {
-
-			result := new(jdoodleResponseResult)
-			err = dec.Decode(result)
-
-			if err != nil {
-				unexpectedError(s, eReact.ChannelID, err)
-				return
-			}
-
-			executor, _ := s.GuildMember(eReact.GuildID, eReact.UserID)
-
-			emb := &discordgo.MessageEmbed{
-				Color: static.ColorEmbedCyan,
-				Title: "Compilation Result",
-				Fields: []*discordgo.MessageEmbedField{
-					{
-						Name:  "Code",
-						Value: fmt.Sprintf("```%s\n%s\n```", embLang, cont),
-					},
-					{
-						Name:  "Output",
-						Value: "```\n" + result.Output + "\n```",
-					},
-					{
-						Name:   "CPU Time",
-						Value:  result.CPUTime + " Seconds",
-						Inline: true,
-					},
-					{
-						Name:   "Memory",
-						Value:  result.Memory + " Byte",
-						Inline: true,
-					},
-				},
-				Footer: &discordgo.MessageEmbedFooter{
-					Text: strings.ToUpper(lang),
-				},
-			}
-
-			if executor != nil {
-				emb.Footer.Text += " | Executed by " + executor.User.String()
-			}
-
-			s.ChannelMessageEditEmbed(resMsg.ChannelID, resMsg.ID, emb)
-		}
+	l.msgMap.Set(e.ID, jdMsg, removeHandlerTimeout, func(v interface{}) {
+		s.MessageReactionRemove(e.ChannelID, e.ID, runReactionEmoji, s.State.User.ID)
 	})
+}
 
-	time.AfterFunc(removeHandlerTimeout, func() {
-		s.MessageReactionsRemoveAll(e.ChannelID, e.ID)
-		removeHandler()
-	})
+func (l *ListenerJdoodle) HandlerReactionAdd(s *discordgo.Session, eReact *discordgo.MessageReactionAdd) {
+	if eReact.UserID == s.State.User.ID {
+		return
+	}
+
+	if eReact.Emoji.Name != runReactionEmoji {
+		return
+	}
+
+	jdMsg, ok := l.msgMap.GetValue(eReact.MessageID).(*jdoodleMessage)
+	if !ok || jdMsg == nil {
+		return
+	}
+
+	if !l.checkLimit(eReact.UserID) {
+		s.MessageReactionRemove(eReact.ChannelID, eReact.MessageID, eReact.Emoji.Name, eReact.UserID)
+		return
+	}
+
+	s.MessageReactionsRemoveAll(eReact.ChannelID, eReact.MessageID)
+
+	resMsg := util.SendEmbed(s, eReact.ChannelID, "Executing...", "", static.ColorEmbedGray)
+	if resMsg.Error() != nil {
+		return
+	}
+
+	bodyBuffer, err := json.Marshal(jdMsg.rb)
+	if err != nil {
+		unexpectedError(s, eReact.ChannelID, err)
+		return
+	}
+	req, err := http.NewRequest("POST", apiURL, bytes.NewReader(bodyBuffer))
+	if err != nil {
+		unexpectedError(s, eReact.ChannelID, err)
+		return
+	}
+	req.Header.Add("Content-Type", "application/json")
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		unexpectedError(s, eReact.ChannelID, err)
+		return
+	}
+
+	dec := json.NewDecoder(res.Body)
+	if res.StatusCode != 200 {
+		errBody := new(jdoodleResponseError)
+		err = dec.Decode(errBody)
+
+		if err != nil {
+			unexpectedError(s, eReact.ChannelID, err)
+			return
+		}
+
+		s.ChannelMessageEditEmbed(resMsg.ChannelID, resMsg.ID, &discordgo.MessageEmbed{
+			Color:       static.ColorEmbedError,
+			Title:       "Execution Error",
+			Description: fmt.Sprintf("API responded with following error: ```\nCode: %d\nMsg:  %s\n```", res.StatusCode, errBody.Error),
+		})
+		discordutil.DeleteMessageLater(s, resMsg.Message, 15*time.Second)
+
+	} else {
+
+		result := new(jdoodleResponseResult)
+		err = dec.Decode(result)
+
+		if err != nil {
+			unexpectedError(s, eReact.ChannelID, err)
+			return
+		}
+
+		executor, _ := s.GuildMember(eReact.GuildID, eReact.UserID)
+
+		emb := &discordgo.MessageEmbed{
+			Color: static.ColorEmbedCyan,
+			Title: "Compilation Result",
+			Fields: []*discordgo.MessageEmbedField{
+				{
+					Name:  "Code",
+					Value: fmt.Sprintf("```%s\n%s\n```", jdMsg.embLang, jdMsg.rb.Script),
+				},
+				{
+					Name:  "Output",
+					Value: "```\n" + result.Output + "\n```",
+				},
+				{
+					Name:   "CPU Time",
+					Value:  result.CPUTime + " Seconds",
+					Inline: true,
+				},
+				{
+					Name:   "Memory",
+					Value:  result.Memory + " Byte",
+					Inline: true,
+				},
+			},
+			Footer: &discordgo.MessageEmbedFooter{
+				Text: strings.ToUpper(jdMsg.rb.Language),
+			},
+		}
+
+		if executor != nil {
+			emb.Footer.Text += " | Executed by " + executor.User.String()
+		}
+
+		s.ChannelMessageEditEmbed(resMsg.ChannelID, resMsg.ID, emb)
+
+		l.msgMap.Remove(jdMsg.ID)
+	}
 }
 
 func (l *ListenerJdoodle) checkLimit(userID string) bool {
