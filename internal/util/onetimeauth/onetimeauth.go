5268b3838f7720b74eafbc1a4ee69376f29baaa0
Ringo Hoffmann <riho@live.de>
2021-03-05 09:01:43 +0100

    make ota tokens truely ot [#206]

--- internal/util/onetimeauth/onetimeauth.go
+++ internal/util/onetimeauth/onetimeauth.go
@@ -8,19 +8,29 @@ import (
 	"github.com/dgrijalva/jwt-go"
 	"github.com/zekroTJA/shinpuru/internal/util"
 	"github.com/zekroTJA/shinpuru/pkg/random"
+	"github.com/zekroTJA/timedmap"
 )
 
 const (
-	keyLength = 128
+	keyLength   = 128
+	tokenLength = 32
 
 	jwtIssuer = "shinpuru v.%v"
 )
 
 var jwtGenerationMethod = jwt.SigningMethodHS256
 
+type otaClaims struct {
+	jwt.StandardClaims
+
+	Token string `json:"tkn"`
+}
+
 type OneTimeAuth struct {
 	signingKey []byte
 	duration   time.Duration
+
+	tokens *timedmap.TimedMap
 }
 
 func New(duration time.Duration) (a *OneTimeAuth, err error) {
@@ -29,7 +39,11 @@ func New(duration time.Duration) (a *OneTimeAuth, err error) {
 		return
 	}
 
-	a = &OneTimeAuth{key, duration}
+	a = &OneTimeAuth{
+		signingKey: key,
+		duration:   duration,
+		tokens:     timedmap.New(10 * time.Minute),
+	}
 
 	return
 }
@@ -37,15 +51,20 @@ func New(duration time.Duration) (a *OneTimeAuth, err error) {
 func (a *OneTimeAuth) GetKey(userID string) (token string, err error) {
 	now := time.Now()
 
-	claims := jwt.StandardClaims{}
+	claims := otaClaims{}
 	claims.Issuer = fmt.Sprintf(jwtIssuer, util.AppVersion)
 	claims.Subject = userID
 	claims.ExpiresAt = now.Add(a.duration).Unix()
 	claims.NotBefore = now.Unix()
 	claims.IssuedAt = now.Unix()
+	if claims.Token, err = random.GetRandBase64Str(32); err != nil {
+		return
+	}
 
 	token, err = jwt.NewWithClaims(jwtGenerationMethod, claims).
 		SignedString(a.signingKey)
+
+	a.tokens.Set(claims.Token, userID, a.duration)
 
 	return
 }
@@ -67,11 +86,19 @@ func (a *OneTimeAuth) ValidateKey(key string) (userID string, err error) {
 		return
 	}
 
-	userID, ok = claims["sub"].(string)
-	if !ok {
+	userID, okS := claims["sub"].(string)
+	tkn, okT := claims["tkn"].(string)
+	if !okS || !okT {
 		err = errors.New("invalid claims")
 		return
 	}
+
+	if uid, ok := a.tokens.GetValue(tkn).(string); !ok || uid != userID {
+		err = errors.New("invalid token")
+		return
+	}
+
+	a.tokens.Remove(tkn)
 
 	return
 }
