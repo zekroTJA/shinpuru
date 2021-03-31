a83acbc2cff8774061534fd0e35c28943929fd8c
Ringo Hoffmann <riho@live.de>
2021-03-31 15:16:12 +0200

    update voicelog listener [#125]

--- internal/core/listeners/voiceupdate.go
+++ internal/core/listeners/voiceupdate.go
@@ -7,7 +7,9 @@ import (
 	"github.com/bwmarrin/discordgo"
 
 	"github.com/zekroTJA/shinpuru/internal/core/database"
+	"github.com/zekroTJA/shinpuru/internal/util"
 	"github.com/zekroTJA/shinpuru/internal/util/static"
+	"github.com/zekroTJA/shinpuru/pkg/discordutil"
 )
 
 var voiceStateCashe = map[string]*discordgo.VoiceState{}
@@ -38,6 +40,29 @@ func (l *ListenerVoiceUpdate) sendVLCMessage(s *discordgo.Session, channelID, us
 	})
 }
 
+func (l *ListenerVoiceUpdate) sendJoinMsg(s *discordgo.Session, voiceLogChan, userID string, newChan *discordgo.Channel) {
+	msgTxt := fmt.Sprintf(":arrow_right:  Joined **`%s`**", newChan.Name)
+	l.sendVLCMessage(s, voiceLogChan, userID, msgTxt, static.ColorEmbedGreen)
+}
+
+func (l *ListenerVoiceUpdate) sendMoveMsg(s *discordgo.Session, voiceLogChan, userID string, oldChan, newChan *discordgo.Channel) {
+	msgTxt := fmt.Sprintf(":left_right_arrow:  Moved from **`%s`** to **`%s`**", oldChan.Name, newChan.Name)
+	l.sendVLCMessage(s, voiceLogChan, userID, msgTxt, static.ColorEmbedCyan)
+}
+
+func (l *ListenerVoiceUpdate) sendLeaveMsg(s *discordgo.Session, voiceLogChan, userID string, oldChan *discordgo.Channel) {
+	msgTxt := fmt.Sprintf(":arrow_left:  Left **`%s`**", oldChan.Name)
+	l.sendVLCMessage(s, voiceLogChan, userID, msgTxt, static.ColorEmbedOrange)
+}
+
+func (l *ListenerVoiceUpdate) isBlocked(guildID, chanID string) (ok bool) {
+	ok, err := l.db.IsGuildVoiceLogIgnored(guildID, chanID)
+	if err != nil {
+		util.Log.Errorf("VOICELOG :: failed getting blocked state: %s", err.Error())
+	}
+	return
+}
+
 func (l *ListenerVoiceUpdate) Handler(s *discordgo.Session, e *discordgo.VoiceStateUpdate) {
 	vsOld, _ := voiceStateCashe[e.UserID]
 	vsNew := e.VoiceState
@@ -52,42 +77,57 @@ func (l *ListenerVoiceUpdate) Handler(s *discordgo.Session, e *discordgo.VoiceSt
 		return
 	}
 
-	_, err = s.Channel(voiceLogChan)
+	_, err = discordutil.GetChannel(s, voiceLogChan)
 	if err != nil {
 		l.db.SetGuildVoiceLog(e.GuildID, "")
 		return
 	}
 
 	if vsOld == nil || (vsOld != nil && vsOld.ChannelID == "") {
-		newChan, err := s.Channel(e.ChannelID)
+		newChan, err := discordutil.GetChannel(s, e.ChannelID)
 		if err != nil {
 			return
 		}
 
-		msgTxt := fmt.Sprintf(":arrow_right:  Joined **`%s`**", newChan.Name)
-		l.sendVLCMessage(s, voiceLogChan, e.UserID, msgTxt, static.ColorEmbedGreen)
+		if l.isBlocked(newChan.GuildID, newChan.ID) {
+			return
+		}
+
+		l.sendJoinMsg(s, voiceLogChan, e.UserID, newChan)
 
 	} else if vsOld != nil && vsNew.ChannelID != "" && vsOld.ChannelID != vsNew.ChannelID {
-		newChan, err := s.Channel(e.ChannelID)
+		newChan, err := discordutil.GetChannel(s, e.ChannelID)
 		if err != nil {
 			return
 		}
 
-		oldChan, err := s.Channel(vsOld.ChannelID)
+		oldChan, err := discordutil.GetChannel(s, vsOld.ChannelID)
 		if err != nil {
 			return
 		}
 
-		msgTxt := fmt.Sprintf(":left_right_arrow:  Moved from **`%s`** to **`%s`**", oldChan.Name, newChan.Name)
-		l.sendVLCMessage(s, voiceLogChan, e.UserID, msgTxt, static.ColorEmbedCyan)
+		newChanBlocked := l.isBlocked(vsNew.GuildID, vsNew.ChannelID)
+		oldChanBlocked := l.isBlocked(vsOld.GuildID, vsOld.ChannelID)
+
+		if newChanBlocked && oldChanBlocked {
+		} else if newChanBlocked {
+			l.sendLeaveMsg(s, voiceLogChan, e.UserID, oldChan)
+		} else if oldChanBlocked {
+			l.sendJoinMsg(s, voiceLogChan, e.UserID, newChan)
+		} else {
+			l.sendMoveMsg(s, voiceLogChan, e.UserID, oldChan, newChan)
+		}
 
 	} else if vsOld != nil && vsNew.ChannelID == "" {
-		oldChan, err := s.Channel(vsOld.ChannelID)
+		oldChan, err := discordutil.GetChannel(s, vsOld.ChannelID)
 		if err != nil {
 			return
 		}
 
-		msgTxt := fmt.Sprintf(":arrow_left:  Left **`%s`**", oldChan.Name)
-		l.sendVLCMessage(s, voiceLogChan, e.UserID, msgTxt, static.ColorEmbedOrange)
+		if l.isBlocked(oldChan.GuildID, oldChan.ID) {
+			return
+		}
+
+		l.sendLeaveMsg(s, voiceLogChan, e.UserID, oldChan)
 	}
 }
