5634f7bcae6498f9bb3f0a8eedea0ce13c2e5472
Ringo Hoffmann <riho@live.de>
2021-08-06 11:13:17 +0200

    check permission of guild admin nav bar entries [#268]

--- web/src/app/components/guild-admin-navbar/guild-admin-navbar.component.ts
+++ web/src/app/components/guild-admin-navbar/guild-admin-navbar.component.ts
@@ -2,12 +2,61 @@
 
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { APIService } from 'src/app/api/api.service';
 
 interface Route {
   route: string;
   icon: string;
   displayname: string;
+  perm?: string;
+  permAny?: string[];
 }
+
+const ROUTES: Route[] = [
+  {
+    route: 'general',
+    icon: 'settings-ol.svg',
+    displayname: 'General',
+    permAny: [
+      'sp.guild.config.autorole',
+      'sp.guild.config.prefix',
+      'sp.guild.config.modlog',
+      'sp.guild.config.voicelog',
+      'sp.guild.config.joinmsg',
+      'sp.guild.config.leavemsg',
+    ],
+  },
+  {
+    route: 'antiraid',
+    icon: 'antiraid.svg',
+    displayname: 'Antiraid',
+    perm: 'sp.guild.config.antiraid',
+  },
+  {
+    route: 'karma',
+    icon: 'karma.svg',
+    displayname: 'Karma',
+    perm: 'sp.guild.config.karma',
+  },
+  {
+    route: 'logs',
+    icon: 'logs.svg',
+    displayname: 'Logs',
+    perm: 'sp.guild.config.logs',
+  },
+  {
+    route: 'data',
+    icon: 'data.svg',
+    displayname: 'Data',
+    perm: 'sp.guild.admin.flushdata',
+  },
+  {
+    route: 'api',
+    icon: 'cloud.svg',
+    displayname: 'API',
+    perm: 'sp.guild.config.api',
+  },
+];
 
 @Component({
   selector: 'app-guild-admin-navbar',
@@ -15,42 +64,27 @@ interface Route {
   styleUrls: ['./guild-admin-navbar.component.scss'],
 })
 export class GuildAdminNavbarComponent implements OnInit {
-  public routes: Route[] = [
-    {
-      route: 'general',
-      icon: 'settings-ol.svg',
-      displayname: 'General',
-    },
-    {
-      route: 'antiraid',
-      icon: 'antiraid.svg',
-      displayname: 'Antiraid',
-    },
-    {
-      route: 'karma',
-      icon: 'karma.svg',
-      displayname: 'Karma',
-    },
-    {
-      route: 'logs',
-      icon: 'logs.svg',
-      displayname: 'Logs',
-    },
-    {
-      route: 'data',
-      icon: 'data.svg',
-      displayname: 'Data',
-    },
-    {
-      route: 'api',
-      icon: 'cloud.svg',
-      displayname: 'API',
-    },
-  ];
+  public routes: Route[] = [];
 
   public currentPath: string;
 
-  constructor(private router: Router, private route: ActivatedRoute) {}
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private api: APIService
+  ) {
+    route.params.subscribe(async (params) => {
+      const guildID = params.guildid;
+      const self = await this.api.getSelfUser().toPromise();
+      const allowed = await api
+        .getPermissionsAllowed(guildID, self.id)
+        .toPromise();
+      this.routes = ROUTES.filter((r) => {
+        if (r.perm) return allowed.includes(r.perm);
+        if (r.permAny) return !!r.permAny.find((p) => allowed.includes(p));
+      });
+    });
+  }
 
   ngOnInit(): void {
     const path = window.location.pathname.split('/');
