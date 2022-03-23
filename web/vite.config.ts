cf9a1651d28dcf5c58f30b954b35232b0c80f3de
Ringo Hoffmann <contact@zekro.de>
2022-03-23 23:59:48 +0100

    update build dist

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,6 +4,9 @@ import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist/web',
+  },
   plugins: [
     react(),
     svgrPlugin({
