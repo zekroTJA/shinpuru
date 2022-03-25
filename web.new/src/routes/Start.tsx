0822f881735910acc53e09d1354940a7cbf2b3ef
Ringo Hoffmann <contact@zekro.de>
2022-03-25 10:27:06 +0100

    add placeholder start page

--- web.new/src/routes/Start.tsx
+++ web.new/src/routes/Start.tsx
@@ -1,5 +1,26 @@
+import { useNavigate } from 'react-router';
+import styled from 'styled-components';
+import { Button } from '../components/Button';
+
 interface Props {}
 
+const StartContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
 export const StartRoute: React.FC<Props> = () => {
-  return <>Start</>;
+  const nav = useNavigate();
+
+  return (
+    <StartContainer>
+      <p>This is only a placeholder start page until a shiny start page is presented here. 😉</p>
+      <Button onClick={() => nav('/login')}>Go to Login</Button>
+    </StartContainer>
+  );
 };
