a750ccda396070de150db4867a57d69354b629eb
zekro <private@zekro.de>
2022-07-11 13:12:06 +0000

    add more general antiraid tests

--- internal/listeners/antiraid_test.go
+++ internal/listeners/antiraid_test.go
@@ -9,31 +9,52 @@ import (
 	"github.com/bwmarrin/discordgo"
 	"github.com/bwmarrin/snowflake"
 	"github.com/sarulabs/di/v2"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/zekroTJA/shinpuru/internal/util/static"
 	"github.com/zekroTJA/shinpuru/mocks"
 )
 
-func TestHandleMemberAdd_Threadsafety(t *testing.T) {
-	sMock := &mocks.ISession{}
-	sMock.On("GuildEdit", mock.Anything, mock.Anything).Return(nil, nil)
-	sMock.On("UserChannelCreate", mock.Anything).Return(&discordgo.Channel{ID: "test-ch"}, nil)
-	sMock.On("ChannelMessageSendEmbed", mock.Anything, mock.Anything).Return(nil, nil)
+type antiraidHandlerMock struct {
+	session *mocks.ISession
+	db      *mocks.Database
+	logger  *mocks.Logger
+	state   *mocks.IState
+	vs      *mocks.VerificationProvider
 
-	dbMock := &mocks.Database{}
-	dbMock.On("GetAntiraidState", mock.Anything).Return(true, nil)
-	dbMock.On("GetAntiraidRegeneration", mock.Anything).Return(30, nil)
-	dbMock.On("GetAntiraidBurst", mock.Anything).Return(50, nil)
-	dbMock.On("GetGuildModLog", mock.Anything).Return("", nil)
-	dbMock.On("GetAntiraidVerification", mock.Anything).Return(false, nil)
-	dbMock.On("AddToAntiraidJoinList", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil)
+	ct di.Container
 
-	glMock := &mocks.Logger{}
-	glMock.On("Errorf", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return()
-	glMock.On("Section", mock.Anything).Return(glMock)
+	getEvent func() *discordgo.GuildMemberAdd
+}
 
-	stMock := &mocks.IState{}
-	stMock.On("Guild", mock.Anything, mock.Anything).Return(&discordgo.Guild{
+func getAntiraidHandlerMock(prep ...func(t antiraidHandlerMock)) antiraidHandlerMock {
+	t := antiraidHandlerMock{}
+
+	t.session = &mocks.ISession{}
+	t.db = &mocks.Database{}
+	t.logger = &mocks.Logger{}
+	t.state = &mocks.IState{}
+	t.vs = &mocks.VerificationProvider{}
+
+	if len(prep) != 0 {
+		prep[0](t)
+	}
+
+	t.session.On("GuildEdit", mock.Anything, mock.Anything).Return(nil, nil)
+	t.session.On("UserChannelCreate", mock.Anything).Return(&discordgo.Channel{ID: "test-ch"}, nil)
+	t.session.On("ChannelMessageSendEmbed", mock.Anything, mock.Anything).Return(nil, nil)
+
+	t.db.On("GetAntiraidState", mock.Anything).Return(true, nil)
+	t.db.On("GetAntiraidRegeneration", mock.Anything).Return(30, nil)
+	t.db.On("GetAntiraidBurst", mock.Anything).Return(3, nil)
+	t.db.On("GetGuildModLog", mock.Anything).Return("", nil)
+	t.db.On("GetAntiraidVerification", mock.Anything).Return(false, nil)
+	t.db.On("AddToAntiraidJoinList", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil)
+
+	t.logger.On("Errorf", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return()
+	t.logger.On("Section", mock.Anything).Return(t.logger)
+
+	t.state.On("Guild", mock.Anything, mock.Anything).Return(&discordgo.Guild{
 		OwnerID: "test-owner",
 		Roles: []*discordgo.Role{
 			{
@@ -42,7 +63,7 @@ func TestHandleMemberAdd_Threadsafety(t *testing.T) {
 			},
 		},
 	}, nil)
-	stMock.On("Members", mock.Anything).Return([]*discordgo.Member{
+	t.state.On("Members", mock.Anything).Return([]*discordgo.Member{
 		{User: &discordgo.User{
 			ID: "test-owner",
 		}},
@@ -54,32 +75,31 @@ func TestHandleMemberAdd_Threadsafety(t *testing.T) {
 		}, Roles: []string{"test-admin-role"}},
 	}, nil)
 
-	vsMock := &mocks.VerificationProvider{}
-	vsMock.On("SetEnabled", mock.Anything, mock.Anything).Return(nil)
+	t.vs.On("SetEnabled", mock.Anything, mock.Anything).Return(nil)
 
 	ct, _ := di.NewBuilder()
 	ct.Add(di.Def{
 		Name:  static.DiDatabase,
-		Build: func(ctn di.Container) (interface{}, error) { return dbMock, nil },
+		Build: func(ctn di.Container) (interface{}, error) { return t.db, nil },
 	})
 	ct.Add(di.Def{
 		Name:  static.DiGuildLog,
-		Build: func(ctn di.Container) (interface{}, error) { return glMock, nil },
+		Build: func(ctn di.Container) (interface{}, error) { return t.logger, nil },
 	})
 	ct.Add(di.Def{
 		Name:  static.DiState,
-		Build: func(ctn di.Container) (interface{}, error) { return stMock, nil },
+		Build: func(ctn di.Container) (interface{}, error) { return t.state, nil },
 	})
 	ct.Add(di.Def{
 		Name:  static.DiVerification,
-		Build: func(ctn di.Container) (interface{}, error) { return vsMock, nil },
+		Build: func(ctn di.Container) (interface{}, error) { return t.vs, nil },
 	})
 
-	l := NewListenerAntiraid(ct.Build())
+	t.ct = ct.Build()
 
 	snowflake.Epoch = 1420070400000
 	node, _ := snowflake.NewNode(0)
-	getEvent := func() *discordgo.GuildMemberAdd {
+	t.getEvent = func() *discordgo.GuildMemberAdd {
 		id := node.Generate().String()
 		return &discordgo.GuildMemberAdd{
 			Member: &discordgo.Member{
@@ -91,7 +111,54 @@ func TestHandleMemberAdd_Threadsafety(t *testing.T) {
 		}
 	}
 
-	const runs = 10000
+	return t
+}
+
+func TestHandleMemberAdd(t *testing.T) {
+	const runs = 10
+
+	collected := make([]string, 0, runs)
+	m := getAntiraidHandlerMock(func(m antiraidHandlerMock) {
+		m.db.On("GetAntiraidRegeneration", mock.Anything).Return(1, nil)
+		m.db.On("GetAntiraidBurst", mock.Anything).Return(3, nil)
+		m.db.On("AddToAntiraidJoinList", mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("time.Time")).
+			Run(func(args mock.Arguments) {
+				assert.Equal(t, args[0], "test-guild")
+				collected = append(collected, args[1].(string))
+			}).
+			Return(nil)
+	})
+
+	l := NewListenerAntiraid(m.ct)
+
+	l.HandlerMemberAdd(m.session, m.getEvent()) // 2 tickets
+	l.HandlerMemberAdd(m.session, m.getEvent()) // 1 ticket
+
+	time.Sleep(1 * time.Second) // 2 tickets
+
+	events := make([]string, 0, runs)
+	for i := 0; i < runs; i++ {
+		e := m.getEvent()
+		if i > 1 {
+			events = append(events, e.User.ID)
+		}
+		l.HandlerMemberAdd(m.session, e)
+	}
+
+	m.session.AssertNumberOfCalls(t, "ChannelMessageSendEmbed", 3)
+	m.db.AssertNumberOfCalls(t, "AddToAntiraidJoinList", runs-3+1)
+
+	assert.ElementsMatch(t, events, collected)
+}
+
+func TestHandleMemberAdd_Threadsafety(t *testing.T) {
+	const runs, burst = 10000, 50
+
+	m := getAntiraidHandlerMock(func(t antiraidHandlerMock) {
+		t.db.On("GetAntiraidBurst", mock.Anything).Return(50, nil)
+	})
+
+	l := NewListenerAntiraid(m.ct)
 
 	var wg sync.WaitGroup
 	wg.Add(runs)
@@ -100,13 +167,13 @@ func TestHandleMemberAdd_Threadsafety(t *testing.T) {
 	for i := 0; i < runs; i++ {
 		go func() {
 			time.Sleep(time.Duration(rand.Intn(1000) * int(time.Millisecond)))
-			l.HandlerMemberAdd(sMock, getEvent())
+			l.HandlerMemberAdd(m.session, m.getEvent())
 			wg.Done()
 		}()
 	}
 
 	wg.Wait()
 
-	sMock.AssertNumberOfCalls(t, "ChannelMessageSendEmbed", 3)
-	dbMock.AssertNumberOfCalls(t, "AddToAntiraidJoinList", runs-50)
+	m.session.AssertNumberOfCalls(t, "ChannelMessageSendEmbed", 3)
+	m.db.AssertNumberOfCalls(t, "AddToAntiraidJoinList", runs-burst)
 }
