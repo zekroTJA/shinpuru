5174350a30eba8299aac1f3054e978490ccf617d
zekro <private@zekro.de>
2022-07-14 10:08:26 +0000

    add tests to verification service

--- internal/services/verification/provider.go
+++ internal/services/verification/provider.go
@@ -1,11 +1,13 @@
 package verification
 
+import "github.com/bwmarrin/discordgo"
+
 type Provider interface {
 	GetEnabled(guildID string) (ok bool, err error)
 	SetEnabled(guildID string, enabled bool) (err error)
 
 	IsVerified(userID string) (ok bool, err error)
-	EnqueueVerification(guildID, userID string) (err error)
+	EnqueueVerification(member discordgo.Member) (err error)
 	Verify(userID string) (err error)
 	KickRoutine()
 }
