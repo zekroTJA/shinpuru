6b9ae0c64d5b4aa9d34bdd074ad722ecf1e63561
zekro <private@zekro.de>
2022-09-01 13:43:20 +0000

    remove mime fix

--- internal/inits/webserver.go
+++ internal/inits/webserver.go
@@ -6,7 +6,6 @@ import (
 	"github.com/zekroTJA/shinpuru/internal/services/config"
 	"github.com/zekroTJA/shinpuru/internal/services/webserver"
 	"github.com/zekroTJA/shinpuru/internal/util/static"
-	"github.com/zekroTJA/shinpuru/pkg/mimefix"
 )
 
 func InitWebServer(container di.Container) (ws *webserver.WebServer) {
@@ -14,17 +13,6 @@ func InitWebServer(container di.Container) (ws *webserver.WebServer) {
 	cfg := container.Get(static.DiConfig).(config.Provider)
 
 	if cfg.Config().WebServer.Enabled {
-		curr, ok := mimefix.Check()
-		if !ok {
-			logrus.Infof("Mime check of .js returned invalid mime value '%s', trying to fix this now ...", curr)
-			if err := mimefix.Fix(); err != nil {
-				logrus.WithError(err).Error("Fixing .js mime value failed (maybe run as admin to fix this)")
-				logrus.Warning("Mime value of .js was not fixed. This may lead to erroneous behaviour of the web server")
-			} else {
-				logrus.Info("Successfully fixed .js mime value")
-			}
-		}
-
 		ws, err := webserver.New(container)
 		if err != nil {
 			logrus.WithError(err).Fatal("Failed initializing web server")
