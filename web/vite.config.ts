c1032aea8f76cee67b244e4353aca4d4119172a0
zekroTJA <riho@live.de>
2023-02-27 15:47:03 +0000

    fix pwa asset caching

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -19,8 +19,42 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,jpeg,jpg}'],
+        globPatterns: ['assets/*.{js,css,html,ico,png,svg,jpeg,jpg}'],
         cacheId: 'shinpuru-v1',
+        runtimeCaching: [
+          {
+            urlPattern: ({ url }) => url.pathname.startsWith('/api'),
+            handler: 'NetworkOnly',
+          },
+        ],
+      },
+      includeAssets: ['favicon.ico', 'logo192.png', 'logo512.png'],
+      manifest: {
+        short_name: 'shinpuru',
+        name: 'shinpuru web interface',
+        description: 'The web interface of the shinpuru Discord bot.',
+        icons: [
+          {
+            src: 'favicon.ico',
+            sizes: '64x64 32x32 24x24 16x16',
+            type: 'image/x-icon',
+          },
+          {
+            src: 'logo192.png',
+            type: 'image/png',
+            sizes: '192x192',
+          },
+          {
+            src: 'logo512.png',
+            type: 'image/png',
+            sizes: '512x512',
+          },
+        ],
+        start_url: '.',
+        display: 'standalone',
+        orientation: 'any',
+        theme_color: '#000000',
+        background_color: '#ffffff',
       },
     }),
   ],
